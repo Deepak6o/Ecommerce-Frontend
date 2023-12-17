91469065b10cf470f396842d2e582c2e280e9ea2
Deepak kumar <deepak21062000@gmail.com>
2023-12-18 00:20:42 +0530

    Added NewCollections component to the Shop page. (generated by blackbox.ai )

--- src/Pages/Shop.jsx
+++ src/Pages/Shop.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import Hero from "../Components/Hero/Hero";
 import Popular from "../Components/Popular/Popular";
 import Offers from "../Components/Offers/Offers";
+import NewCollections from "../Components/NewCollections/NewCollections";
+import NewsLetter from "../Components/NewsLetter/NewsLetter";
 
 const Shop = () => {
   return (
@@ -9,6 +11,8 @@ const Shop = () => {
       <Hero />
       <Popular />
       <Offers/>
+      <NewCollections/>
+      <NewsLetter/>
     </div>
   );
 };
