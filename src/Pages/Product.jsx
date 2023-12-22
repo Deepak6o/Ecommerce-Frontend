94c20e255bb4ced7d8760f2bb27e324c251b00b3
Deepak kumar <deepak21062000@gmail.com>
2023-12-22 12:58:16 +0530

    product

--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -2,6 +2,7 @@ import React, { useContext } from 'react'
 import { ShopContext } from '../Context/ShopContext'
 import { useParams } from 'react-router-dom';
 import Breadcrum from '../Components/Breadcrums/Breadcrum';
+import ProductDisplay from '../Components/ProductDisplay/ProductDisplay';
 
 const Product = () => {
   const {all_product} = useContext(ShopContext);
@@ -10,6 +11,7 @@ const Product = () => {
   return (
     <div>
       <Breadcrum product={product}/>
+      <ProductDisplay product={product}/>
     </div>
   )
 }
